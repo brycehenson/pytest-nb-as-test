f2e5ed868589260cea69360cc7fd3d2cbc85deaf
bryce <bryce>
2026-01-14 00:36:26 +0000

    fixed no id

--- tools/fix_notebook_cell_id.py
+++ tools/fix_notebook_cell_id.py
@@ -1,10 +1,11 @@
-import json
+"""Normalize notebook cell ids for test notebooks."""
+
 from pathlib import Path
 
 import nbformat
 from nbformat import validator
 
-for path in Path("tests/notebooks").glob("*.ipynb"):
+for path in Path("tests").rglob("*.ipynb"):
     nb = nbformat.read(path, as_version=4)
     validator.normalize(nb)
     nbformat.write(nb, path)
